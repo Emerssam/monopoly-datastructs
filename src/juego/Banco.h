ff11bce4c79be20d345dadad518ebc8c6ee248ca
Emerson David Perea <emersonperea00@gmail.com>
2025-11-12 11:16:03 -0500

    Implementacion de juego

--- src/juego/Banco.h
+++ src/juego/Banco.h
@@ -1,4 +1,21 @@
-#ifndef BANCO_H
-#define BANCO_H
+#ifndef "BANCO_H"
+#define "BANCO_H"
 
-#endif //BANCO_H
+#include <string>
+#include <unordered_map>
+
+class Banco {
+private:
+    std::unordered_map<std::string, int> cuentas;
+
+public:
+    Banco();
+
+    void crearCuenta(const std::string& jugador, int montoInicial = 1500);
+    bool transFerir(const std::string& origen, const std::string& destino, int monto);
+    bool pagarBanco(const std::string& jugador, int monto);
+    bool recibirBanco(const std::string& jugador, int monto);
+    int gtSaldo(const std::string& jugador) const;
+
+};
+#endif
