3c5f9a3d5e124e4d6954601fc74aa6add73836e3
Emerson David Perea <emersonperea00@gmail.com>
2025-11-12 11:34:39 -0500

    Implementacion de juego

--- src/juego/Banco.cpp
+++ src/juego/Banco.cpp
@@ -1,1 +1,29 @@
 #include "Banco.h"
+#include <iostream>
+
+Banco::Banco() {}
+
+void Banco::crearCuenta(const std::string& jugador, int MontoInicial) {
+  cuentas[jugador] = MontoInicial;
+}
+
+bool Banco::transFerir(const std::string& origen, const std::string& destino, int monto); {
+  if cuentas[origen] < monto) return false;
+  cuentas[origen] -= monto;
+  cuentas [destino] += monto;
+  return true;
+}
+
+bool Banco::pagarBanco(const std::string& jugador, int monto); {
+  if (cuentas[jugador] < monto) return false;
+  cuantas[jugador] -= monto;
+  return true;
+}
+
+int Banco::gtSaldo(const std::string& jugador) const; {
+  auto it = cuentas.find(jugador);
+  if (it != cuentas.end())
+    return it->second;
+  return 0;
+
+}
