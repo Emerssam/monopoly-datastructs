fe6daf291afb1b152fd97994978258f7aaa70a83
Emerson David Perea <emersonperea00@gmail.com>
2025-11-16 23:37:33 -0500

    Implementacion de juego

--- src/juego/Juego.h
+++ src/juego/Juego.h
@@ -1,4 +1,26 @@
 #ifndef JUEGO_H
 #define JUEGO_H
 
-#endif //JUEGO_H
+#include "Banco.h"
+#include "Dado.h"
+#include "Estado.h"
+#include "Reglas.h"
+#include "Turnos.h"
+
+class Juego {
+private:
+    Banco banco;
+    Dado dado1, dado2;
+    Estado estado;
+    Reglas reglas;
+    Turnos turnos;
+
+public:
+    Juego();
+    void inicializarJugadores();
+    void jugarTurno();
+    void mostrarEstado();
+    bool haTerminado();
+};
+
+#endif
